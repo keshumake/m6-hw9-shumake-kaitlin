4a036b19895338071f93c4994c0bcc52df02a6c6
Kaitlin Shumake <shumakek@ufl.edu>
2022-03-27 11:46:09 -0400

    added all result elements

--- js/index.js
+++ js/index.js
@@ -7,10 +7,9 @@ btn.onclick = function() {
         return weatherResponse.json()
     })
     .then(function(weatherData) {
-        console.log(weatherData)
+        weatherEl.innerHTML= ""
         weatherData.results.forEach(function(searchweather) {
             var weatherDiv = document.createElement('div')
-            
             
             var location = document.createElement('h3')
             location.textContent = searchweather.name + ", " + searchweather.sys.country
@@ -20,9 +19,18 @@ btn.onclick = function() {
             weatherResult.textContent = searchweather.weather.description
             weatherDiv.appendChild(weatherResult)
 
-            var current = document. createElement('h3')
+            var current = document.createElement('h3')
             current.textContent = "Current: " + searchweather.main.temp
             weatherDiv.appendChild(current)
+
+            var feels = document.createElement('h3')
+            feels.textContent = "Feels like: " + searchweather.main.feels_like
+            weatherDiv.appendChild(feels)
+
+            var updated = document.createElement('h3')
+            var date = dateNow()
+            updated.textContent = "Last updated: " + date
+            weatherDiv.appendChild(updated)
 
 
             weatherEl.appendChild(weatherDiv)
