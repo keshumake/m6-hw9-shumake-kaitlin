0f5cac4182386b56da7c0b114fa84a587e5d669e
Kaitlin Shumake <shumakek@ufl.edu>
2022-03-28 21:31:58 -0400

    fixed errors and corrected scopes

--- js/index.js
+++ js/index.js
@@ -6,50 +6,55 @@ formEl.onsubmit = function(e) {
   e.preventDefault()
   console.log(searchweather.value)
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchweather.value}&appid=f2fc7aa133392d3242a22babdd19e61b`)
-    .then (function(weatherEl) {
-        return weatherEl.json()
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchweather.value}&appid=f2fc7aa133392d3242a22babdd19e61b&units=imperial`)
+    .then (function(fetchedResponse) {
+        return fetchedResponse.json()
     })
-    .then(function(weatherEl) {
-        console.log(weatherEl)
-        weatherEl.value = ""
+    .then(function(jsonResponse) {
+        console.log(jsonResponse)
+        var weatherDiv2 = document.getElementById('weather')
+        weatherDiv2.appendChild(renderWeather(jsonResponse))
     })
     .catch(function(error) {
         console.log(error)
-        weatherEl.innerHTML = "Please enter another location."
+        var error = document.getElementById('searchweather')
+        error.innerHTML = error.message
     })
 }
 
 
 
-function renderUsers(searchweather) {
-        weatherEl.innerHTML= ""
-        searchweather.forEach(searchweather => {
+function renderWeather(searchweather) {
             var weatherDiv = document.createElement('div')
             
-            var location = document.createElement('h3')
+            var location = document.createElement('h2')
             location.textContent = searchweather.name + ", " + searchweather.sys.country
             weatherDiv.appendChild(location)
+
+            var pic = document.createElement('img')
+            pic.src = searchweather.weather.icon
+            weatherDiv.appendChild(pic)
 
             var weatherResult = document.createElement('h3')
             weatherResult.textContent = searchweather.weather.description
             weatherDiv.appendChild(weatherResult)
 
             var current = document.createElement('h3')
-            current.textContent = "Current: " + searchweather.main.temp
+            current.textContent = "Current: " + Math.round(searchweather.main.temp) + " ° F"
             weatherDiv.appendChild(current)
 
             var feels = document.createElement('h3')
-            feels.textContent = "Feels like: " + searchweather.main.feels_like
+            feels.textContent = "Feels like: " + Math.round(searchweather.main.feels_like) + " ° F"
             weatherDiv.appendChild(feels)
 
             var updated = document.createElement('h3')
-            var date = dateNow()
-            updated.textContent = "Last updated: " + date
+            var date = new Date()
+            var timeOfDay = date.getHours() >= 12 ? 'pm' : 'am';
+            var time = date.getHours() + ":" + date.getMinutes() + " " + timeOfDay
+            updated.textContent = "Last updated: " + time
             weatherDiv.appendChild(updated)
 
+            return weatherDiv
 
-            weatherEl.appendChild(weatherDiv)
-        });
-        
-}
+        }
+    
